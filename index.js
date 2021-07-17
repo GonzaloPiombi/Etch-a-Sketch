ca52900d5af30ee949abf2c18306c4cbe1a41ef9
Gonzalo <gonzalopiombi@gmail.com>
2021-07-17 16:11:08 -0300

    Add basic 16x16 grid. Add possibility of choosing between painting the squares black, with a random rgb value, and a progressive scale of gray.

---
+++ index.js
@@ -0,0 +1,48 @@
+const buttons = document.querySelectorAll('.color-buttons');
+const square = document.querySelector('.grid-container').childNodes;
+let colorMode = '';
+
+for (let i = 0; i < 256; i++) {
+    const newDiv = document.createElement('div');
+    document.querySelector('.grid-container').appendChild(newDiv);
+}
+
+
+buttons.forEach((button => {
+    button.addEventListener('click', changeColor);
+}));
+
+function changeColor() {
+    colorMode = this.id;
+    square.forEach((div => {
+        div.addEventListener('mouseover', changeColorMode);
+    }));
+}
+
+function changeColorMode() {
+    switch (colorMode) {
+        case 'black':
+            this.setAttribute('style', 'background-color: black;');
+            break;
+
+        case 'rgb':
+            this.setAttribute('style', `background-color: rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)});`);
+            break;
+
+        case 'gray':
+            let currentValue = this.style.backgroundColor;
+            currentValue = currentValue.slice(-5, -1);
+            currentValue = Number(currentValue);
+            if (currentValue <= 1) {
+                this.setAttribute('style', `background-color: rgba(0, 0, 0, ${currentValue + 0.1})`)
+            } else {
+                this.setAttribute('style', `background-color: rgba(0, 0, 0, 1);`)
+
+            };
+            break;
+
+        case 'eraser':
+            this.setAttribute('style', 'background-color: white;')
+            break;
+    }
+}
