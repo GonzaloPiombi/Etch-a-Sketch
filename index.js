6796237fceedd93451aa61f8e64c6d6537091418
Gonzalo <gonzalopiombi@gmail.com>
2021-07-19 12:43:08 -0300

    Add button to clear the grid, and button that prompts for new grid size and generates it within 480x480px

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const buttons = document.querySelectorAll('.color-buttons');
 const square = document.querySelector('.grid-container').childNodes;
+const clearButton = document.querySelectorAll('button')[4];
+const newGridButton = document.querySelectorAll('button')[5];
 let colorMode = '';
 
 for (let i = 0; i < 256; i++) {
@@ -7,6 +9,8 @@ for (let i = 0; i < 256; i++) {
     document.querySelector('.grid-container').appendChild(newDiv);
 }
 
+clearButton.addEventListener('click', clearGrid);
+newGridButton.addEventListener('click', newGrid);
 
 buttons.forEach((button => {
     button.addEventListener('click', changeColor);
@@ -47,5 +51,26 @@ function changeColorMode() {
         case 'eraser':
             this.setAttribute('style', 'background-color: ;')
             break;
+    }
+}
+
+function clearGrid() {
+    square.forEach((div => {
+        div.setAttribute('style', 'background-color: white;');
+    }));
+}
+
+function newGrid() {
+    const gridContainer = document.querySelector('.grid-container');
+
+    do {
+        number = parseInt(prompt('Enter grid size:', 16), 10);
+    } while (number > 100 || number < 1 || isNaN(number));
+
+    gridContainer.textContent = '';
+    gridContainer.setAttribute('style', `grid-template-columns: repeat(${number}, ${480/number}px); grid-template-rows: repeat(${number}, ${480/number}px);`)
+    for (let i = 0; i < number*number; i++) {
+        const newDiv = document.createElement('div')
+        gridContainer.appendChild(newDiv);
     }
 }
